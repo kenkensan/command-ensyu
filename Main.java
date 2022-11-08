e445ec1d10938a7b5397c89ecf05a51a0a2f243d
tanaka ken <k.tanaka.sys22@morijyobi.ac.jp>
2022-11-08 09:56:30 +0900

    Main.javaの作成

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+	public static void main (String[]args){
+		double hight=160;
+		double weight=57;
+		double hightm=hight/100;
+		double bmi=weight/(hightm*hightm);
+	System.out.println("Ç†Ç»ÇΩÇÃBMIÇÕ"+String.format("%.2f",bmi)+"Ç≈Ç∑ÅB");
+	}
+}
