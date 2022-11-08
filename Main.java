4ba855e3bd02002239cb697cd9557be91bb9eaf6
tanaka ken <k.tanaka.sys22@morijyobi.ac.jp>
2022-11-08 10:14:29 +0900

    aaaaaa

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 public class Main{
 	public static void main (String[]args){
-		double hight=160;
-		double weight=57;
-		double hightm=hight/100;
-		double bmi=weight/(hightm*hightm);
-	System.out.println("あなたのBMIは"+String.format("%.2f",bmi)+"です。");
+		System.out.print("1人目の身長:");
+		double hight1=new java.util.Scanner(System.in).nextInt();
+		System.out.print("1人目の体重:");
+		double weight1=new java.util.Scanner(System.in).nextInt();
+		System.out.print("2人目の身長:");
+		double hight2=new java.util.Scanner(System.in).nextInt();
+		System.out.print("2人目の体重:");
+		double weight2=new java.util.Scanner(System.in).nextInt();
+		double hightm=hight1/100;
+		double hightm2=hight2/100;
+		double bmi=weight1/(hightm*hightm);
+		double bmi2=weight2/(hightm2*hightm2);
+	System.out.println("あなたたちのBMIは"+String.format("%.2f",bmi)+"と,"+String.format("%.2f",bmi2)+"です。");
 	}
 }
